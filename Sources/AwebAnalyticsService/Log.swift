87fbb80b7338f1e0dbc569712f29a5d4344e1cc3
oleksii savchenko <osavchenko@competo.io>
2026-06-10 16:33:27 +0300

    Added sentry logging

--- Sources/AwebAnalyticsService/Log.swift
+++ Sources/AwebAnalyticsService/Log.swift
@@ -1,5 +1,6 @@
 import Foundation
 import os
+import Sentry
 
 enum Log {
     
@@ -17,11 +18,27 @@ enum Log {
             case .error: return "[ERROR]: "
             }
         }
+        
+        var sentryLevel: SentryLevel {
+            switch self {
+            case .analytics: return .info
+            case .debug: return .debug
+            case .error: return .error
+            }
+        }
     }
     
     static func printLog(l: Level, str: String) {
         let message = "\(l.prefix)\(str)"
         print(message)
         logger.notice("\(message, privacy: .public)")
+        
+        let breadcrumb = Breadcrumb(level: l.sentryLevel, category: "log")
+        breadcrumb.message = message
+        SentrySDK.addBreadcrumb(breadcrumb)
+        
+        if l == .error {
+            SentrySDK.capture(message: message)
+        }
     }
 }
